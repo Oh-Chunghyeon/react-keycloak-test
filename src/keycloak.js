5539218f77b17a96dff94796a26b021378a65c42
Oh-Chunghyeon <sean@bliex.com>
2022-03-11 14:38:15 +0900

    업데이트

--- src/keycloak.js
+++ src/keycloak.js
@@ -6,16 +6,16 @@ import Keycloak from "keycloak-js";
 //   clientId: "react-auth",
 // });
 
-// const keycloak = new Keycloak({
-//   url: "http://localhost:8080/auth",
-//   realm: "ReactKeyCloak",
-//   clientId: "react-login",
-// });
-
 const keycloak = new Keycloak({
-  url: "http://dev-keycloak.pnpt.net:8080/",
-  realm: "pinpoint",
-  clientId: "ppnt-login",
+  url: "http://localhost:8080/auth",
+  realm: "ppoint",
+  clientId: "ppoint-auth",
 });
+
+// const keycloak = new Keycloak({
+//   url: "http://dev-keycloak.pnpt.net:8080/",
+//   realm: "pinpoint",
+//   clientId: "ppnt-login",
+// });
 
 export default keycloak;
