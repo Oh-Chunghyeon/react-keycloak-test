93407b74410185e1a9bb305c8da55283ddd5e75c
Oh-Chunghyeon <sean@bliex.com>
2022-03-11 15:40:43 +0900

    update

--- src/pages/Secured.jsx
+++ src/pages/Secured.jsx
@@ -1,9 +1,32 @@
+import { useKeycloak } from "@react-keycloak/web";
 import React from "react";
+import "../styles/secured.scss";
 
 const Secured = () => {
+  const { keycloak, initialized } = useKeycloak();
+
+  if (!initialized) {
+    return <h1>Loading...</h1>;
+  }
+
+  console.log(keycloak);
+
   return (
-    <div>
+    <div className="secured-container">
       <h1 className="text-black text-4xl">Welcome to the Protected Page.</h1>
+
+      <div className="sub-title">
+        <h2>User Infomation</h2>
+      </div>
+
+      <ul className="user-info">
+        <li>
+          <span>user name : {keycloak.tokenParsed.name}</span>
+        </li>
+        <li>
+          <span>user email : {keycloak.tokenParsed.email}</span>
+        </li>
+      </ul>
     </div>
   );
 };
