c9c4feb8483b3e96f6a2de72772a63e06d2cc227
Oh-Chunghyeon <sean@bliex.com>
2022-03-11 16:43:09 +0900

    update

--- src/pages/Secured.jsx
+++ src/pages/Secured.jsx
@@ -5,28 +5,34 @@ import "../styles/secured.scss";
 const Secured = () => {
   const { keycloak, initialized } = useKeycloak();
 
-  if (!initialized) {
-    return <h1>Loading...</h1>;
-  }
-
   console.log(keycloak);
 
   return (
     <div className="secured-container">
-      <h1 className="text-black text-4xl">Welcome to the Protected Page.</h1>
+      {!initialized ? (
+        <>
+          <h1>로그인 정보가 있으면 확인가능합니다.</h1>;
+        </>
+      ) : (
+        <>
+          <h1 className="text-black text-4xl">
+            Welcome to the Protected Page.
+          </h1>
 
-      <div className="sub-title">
-        <h2>User Infomation</h2>
-      </div>
+          <div className="sub-title">
+            <h2>User Infomation</h2>
+          </div>
 
-      <ul className="user-info">
-        <li>
-          <span>user name : {keycloak.tokenParsed.name}</span>
-        </li>
-        <li>
-          <span>user email : {keycloak.tokenParsed.email}</span>
-        </li>
-      </ul>
+          <ul className="user-info">
+            <li>
+              <span>user name : {keycloak.tokenParsed.name}</span>
+            </li>
+            <li>
+              <span>user email : {keycloak.tokenParsed.email}</span>
+            </li>
+          </ul>
+        </>
+      )}
     </div>
   );
 };
